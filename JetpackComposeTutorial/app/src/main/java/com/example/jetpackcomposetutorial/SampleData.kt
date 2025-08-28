995dc95f7b3e1f0b72fab65bee1ed47185bb0886
Shihab <87382972+ShihabHT@users.noreply.github.com>
2025-08-28 18:12:46 +0600

    conversation, and animation

---
+++ JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/SampleData.kt
@@ -0,0 +1,28 @@
+package com.example.jetpackcomposetutorial // Use your own package name
+
+object SampleData {
+    // A list of sample messages
+    val conversationSample = listOf(
+        Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!"),
+        Message("Lexi", "It's so much fun to build UIs with it. Look at this list I made."),
+        Message("Lexi", "We can make it dynamic and scrollable..."),
+        Message("Lexi", "And add images, shapes, and borders easily."),
+        Message("Lexi", "This message is a bit longer to see how the text wraps inside the message bubble. It should handle multiple lines gracefully."),
+        Message("Lexi", "Let's add one more for good measure."),
+        Message("Lexi", "Okay, this is the last one. Bye!"),
+        Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!"),
+        Message("Lexi", "It's so much fun to build UIs with it. Look at this list I made."),
+        Message("Lexi", "We can make it dynamic and scrollable..."),
+        Message("Lexi", "And add images, shapes, and borders easily."),
+        Message("Lexi", "This message is a bit longer to see how the text wraps inside the message bubble. It should handle multiple lines gracefully."),
+        Message("Lexi", "Let's add one more for good measure."),
+        Message("Lexi", "Okay, this is the last one. Bye!"),
+        Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!"),
+        Message("Lexi", "It's so much fun to build UIs with it. Look at this list I made."),
+        Message("Lexi", "We can make it dynamic and scrollable..."),
+        Message("Lexi", "And add images, shapes, and borders easily."),
+        Message("Lexi", "This message is a bit longer to see how the text wraps inside the message bubble. It should handle multiple lines gracefully."),
+        Message("Lexi", "Let's add one more for good measure."),
+        Message("Lexi", "Okay, this is the last one. Bye!")
+    )
+}

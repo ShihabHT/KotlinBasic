779dfb99931d15b377a80a89dc14de534e94f79a
Shihab <87382972+ShihabHT@users.noreply.github.com>
2025-09-05 20:19:03 +0600

    added toggle switch

--- JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/SettingsActivity.kt
+++ JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/SettingsActivity.kt
@@ -2,15 +2,23 @@ package com.example.jetpackcomposetutorial
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.Switch
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -31,6 +39,8 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 import kotlinx.coroutines.flow.Flow
 
@@ -46,9 +56,13 @@ class SettingsActivity : ComponentActivity() {
                 val context = LocalContext.current
                 val viewModel: SettingsViewModel = viewModel(factory = SettingsViewModelFactory(context.dataStore))
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        viewModel = viewModel,modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Greeting( viewModel = viewModel)
+
+                        Spacer(modifier = Modifier.height(5.dp))
+                        NotificationEnabledSection(viewModel)
+                    }
                 }
             }
         }
@@ -56,28 +70,45 @@ class SettingsActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(viewModel: SettingsViewModel, modifier: Modifier = Modifier) {
+fun Greeting(viewModel: SettingsViewModel) {
     val focusManager = LocalFocusManager.current // to remove focus from TextField
-    val userName by viewModel.userNameFlow.collectAsState(initial = "Loading...")
+    val userName by viewModel.userNameFlow.collectAsState(initial = "...")
     var text by remember { mutableStateOf("") }
 
-//    LaunchedEffect(key1 = userName){
-//        text = userName
-//    }
-    Column {
-        Text("The text is-> $userName", modifier = modifier)
+    LaunchedEffect(key1 = userName){
+        text = userName
+    }
+
+    Row(modifier = Modifier.padding(horizontal = 10.dp)) {
+        Text("-> $userName", modifier = Modifier.background(color = MaterialTheme.colorScheme.primary))
         TextField(
             value = text,
             onValueChange = { text = it },
-            label = { Text("Enter your name") },
+//            label = { Text("Enter your name") },
             singleLine = true,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.weight(1f) // Gives this element the most available space
         )
         Button(onClick = {
             viewModel.saveUserName(text)
-            text = ""
+//            text = ""
             focusManager.clearFocus()
         }) { Text(text = "Save") }
+    }
+}
+
+@Composable
+fun NotificationEnabledSection(viewModel: SettingsViewModel, modifier: Modifier = Modifier){
+    val notificationEnabled by viewModel.notificationEnabledFlow.collectAsState(initial = true)
+
+    Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp)) {
+        Text(text = "$notificationEnabled")
+        Spacer(modifier = Modifier.weight(1f))
+        Switch(
+            checked = notificationEnabled,
+            onCheckedChange = {
+                viewModel.saveNotificationEnabled(it)
+            }
+        )
     }
 }
 
@@ -86,6 +117,9 @@ fun Greeting(viewModel: SettingsViewModel, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     JetpackComposeTutorialTheme {
         val viewModel: SettingsViewModel = viewModel(factory = SettingsViewModelFactory(LocalContext.current.dataStore))
-        Greeting(viewModel = viewModel, modifier = Modifier)
+        Column {
+            Greeting(viewModel = viewModel)
+            NotificationEnabledSection(viewModel)
+        }
     }
 }
--- JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/SettingsViewModel.kt
+++ JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/SettingsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.*
@@ -15,14 +16,16 @@ class SettingsViewModel(
 
     // Define the key and default value in the ViewModel
     private val USER_NAME_KEY = stringPreferencesKey("user_name")
-    private val DEFAULT_USER_NAME = "Android"
+    private val NOTIFICATION_ENABLED_KEY = booleanPreferencesKey("notification_enabled")
 
-    // Expose the Flow from DataStore
+    private val DEFAULT_USER_NAME = "Android"
+    private val DEFAULT_NOTIFICATION_ENABLED = true
+
+    // Expose the userName key Flow from DataStore
     val userNameFlow: Flow<String> = dataStore.data.map { preferences ->
         preferences[USER_NAME_KEY] ?: DEFAULT_USER_NAME
     }
-
-    // A suspend function to save the data
+    // A suspend function to save the userName
     fun saveUserName(name: String) {
         viewModelScope.launch {
             dataStore.edit { preferences ->
@@ -30,4 +33,18 @@ class SettingsViewModel(
             }
         }
     }
+
+    // Expose notificationEnabled key Flow from DataStore
+    val notificationEnabledFlow: Flow<Boolean> = dataStore.data.map { preferences ->
+        preferences[NOTIFICATION_ENABLED_KEY] ?: DEFAULT_NOTIFICATION_ENABLED
+    }
+    // a suspend function to save the notificationEnabled
+    fun saveNotificationEnabled(enabled: Boolean) {
+        viewModelScope.launch {
+            dataStore.edit { preferences ->
+                preferences[NOTIFICATION_ENABLED_KEY] = enabled
+            }
+        }
+    }
+
 }
